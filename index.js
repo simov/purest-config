925e364d00e6f8abc87c0d66cc59a5ec670ab9b9
simov <simeonvelichkov@gmail.com>
2016-02-09 21:14:28 +0200

    Major refactoring

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-module.exports = {
-  init: require('./lib/init'),
-  endpoint: require('./lib/endpoint'),
-  url: require('./lib/url')
+var init = require('./lib/init')
+var endpoint = require('./lib/endpoint')
+var url = require('./lib/url')
+
+
+module.exports = (instance, config, extend) => {
+
+  var aliases = init.aliases(config)
+
+  return {
+    endpoint: endpoint(aliases, extend),
+    url: url(instance, aliases),
+    aliases: aliases
+  }
 }
