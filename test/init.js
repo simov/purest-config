9d57c3fc254248a1ba8a1a35a38adad346326383
simov <simeonvelichkov@gmail.com>
2016-04-10 12:20:18 +0300

    Improve init tests

--- test/init.js
+++ test/init.js
@@ -1,135 +1,149 @@
+'use strict'
 
-var should = require('should')
+var t = require('assert')
 var init = require('../lib/init')
 
 
-describe('aliases', function () {
-  it('throw on missing __path key', function () {
+describe('alias', () => {
+  it('__domain', () => {
     var provider = {
-      domain: {
-        path: {}
+      domain1: {
+        __domain: {subdomain: 'subdomain1', auth: {a: 1}},
+        path1: {}
       }
     }
-    ;(function () {
-      var aliases = init.aliases(provider)
-    }).should.throw('Purest: __path key is required!')
-  })
-  it('throw on missing __path.alias key', function () {
-    var provider = {
-      domain: {
-        path: {__path:{}}
-      }
-    }
-    ;(function () {
-      var aliases = init.aliases(provider)
-    }).should.throw('Purest: __path.alias key is required!')
-  })
-  it('alias string', function () {
-    var provider = {
-      domain: {
-        path: {__path: {alias:'name'}}
-      }
-    }
-    var aliases = init.aliases(provider)
-    should.deepEqual(aliases, {
-      name: {
-        domain:'domain', path:'path'
-      }
+    var config = init.alias(provider, 'domain1', 'path1')
+    t.deepEqual(config, {
+      domain: 'domain1',
+      path: 'path1',
+      subdomain: 'subdomain1',
+      auth: {a: 1}
     })
   })
-  it('alias array', function () {
+  it('__path', () => {
     var provider = {
-      domain: {
-        path: {__path: {alias:['name1', 'name2']}}
+      domain1: {
+        __domain: {subdomain: 'subdomain1', auth: {a: 1}},
+        path1: {
+          __path: {
+            subdomain: 'subdomain2',
+            subpath: 'subpath1',
+            version: 'v1',
+            endpoint: 'endpoint1',
+            auth: {b: 2}
+          }
+        }
       }
     }
-    var aliases = init.aliases(provider)
-    should.deepEqual(aliases, {
-      name1: {
-        domain:'domain', path:'path'
-      },
-      name2: {
-        domain:'domain', path:'path'
-      }
+    var config = init.alias(provider, 'domain1', 'path1')
+    t.deepEqual(config, {
+      domain: 'domain1',
+      path: 'path1',
+      subdomain: 'subdomain2',
+      subpath: 'subpath1',
+      version: 'v1',
+      endpoint: 'endpoint1',
+      auth: {b: 2}
     })
   })
-  it('__path.version', function () {
-    var provider = {
-      domain: {
-        path: {__path: {alias:'name', version:'v1'}}
-      }
-    }
-    var aliases = init.aliases(provider)
-    should.deepEqual(aliases, {
-      name: {
-        domain:'domain', path:'path', version:'v1'
-      }
-    })
+})
+
+describe('endpoints', () => {
+  it('no endpoints', () => {
+    var provider = {domain1: {path1: {}}}
+    var config = init.endpoints(provider, 'domain1', 'path1')
+    t.equal(config, undefined)
   })
-  it('__domain.auth', function () {
+  it('all, str, regex', () => {
     var provider = {
-      domain: {
-        __domain: {auth:{a:1}},
-        path: {__path: {alias:'name'}}
+      domain1: {
+        path1: {
+          '*': {get: {a: 1}},
+          'some/path': {post: {b: 2}},
+          'id\/\\d+': {__endpoint: {regex: true}, all: {c: 3}}
+        }
       }
     }
-    var aliases = init.aliases(provider)
-    should.deepEqual(aliases, {
-      name: {
-        domain:'domain', path:'path', auth:{a:1}
-      }
-    })
-  })
-  it('__path.auth', function () {
-    var provider = {
-      domain: {
-        __domain: {auth:{a:1}},
-        path: {__path: {alias:'name', auth:{a:2}}}
-      }
-    }
-    var aliases = init.aliases(provider)
-    should.deepEqual(aliases, {
-      name: {
-        domain:'domain', path:'path', auth:{a:2}
-      }
+    var config = init.endpoints(provider, 'domain1', 'path1')
+    t.deepEqual(config, {
+      all: {get: {a: 1}},
+      str: {'some/path': {post: {b: 2}}},
+      regex: {'id\/\\d+': {__endpoint: {regex: true}, all: {c: 3}}}
     })
   })
 })
 
 
-describe('endpoints', function () {
-  it('all', function () {
+describe('aliases', () => {
+  it('throw on missing __path key', () => {
     var provider = {
-      domain: {
-        path: {
-          '*': {get:{a:1}}
-        }
-      }
+      domain: {path: {}}
     }
-    var result = init.endpoints(provider, 'domain', 'path')
-    should.deepEqual(result, {all: {get:{a:1}}})
+    t.throws(() => {
+      var aliases = init.aliases(provider)
+    }, 'Purest: __path key is required!')
   })
-  it('regex', function () {
+  it('throw on missing __path.alias key', () => {
     var provider = {
-      domain: {
-        path: {
-          '\\d+': {__endpoint:{regex:true}, get:{a:1}}
-        }
-      }
+      domain: {path: {__path: {}}}
     }
-    var result = init.endpoints(provider, 'domain', 'path')
-    should.deepEqual(result, {
-      regex: {'\\d+': {__endpoint:{regex:true}, get:{a:1}}}})
+    t.throws(() => {
+      var aliases = init.aliases(provider)
+    }, 'Purest: __path.alias key is required!')
   })
-  it('str', function () {
+
+  it('alias string', () => {
     var provider = {
-      domain: {
-        path: {
-          'string': {get:{a:1}}
-        }
+      domain1: {path1: {__path: {alias: 'name1'}}}
+    }
+    var aliases = init.aliases(provider)
+    t.deepEqual(aliases, {
+      name1: {domain: 'domain1', path: 'path1'}
+    })
+  })
+  it('alias array', () => {
+    var provider = {
+      domain1: {path1: {__path: {alias: ['name1', 'name2']}}}
+    }
+    var aliases = init.aliases(provider)
+    t.deepEqual(aliases, {
+      name1: {domain: 'domain1', path: 'path1'},
+      name2: {domain: 'domain1', path: 'path1'}
+    })
+  })
+
+  it('__path.version', () => {
+    var provider = {
+      domain1: {path1: {__path: {alias: 'name1', version: 'v1'}}}
+    }
+    var aliases = init.aliases(provider)
+    t.deepEqual(aliases, {
+      name1: {domain: 'domain1', path: 'path1', version: 'v1'}
+    })
+  })
+
+  it('__domain.auth', () => {
+    var provider = {
+      domain1: {
+        __domain: {auth: {a: 1}},
+        path1: {__path: {alias: 'name1'}}
       }
     }
-    var result = init.endpoints(provider, 'domain', 'path')
-    should.deepEqual(result, {str:{string:{get:{a:1}}}})
+    var aliases = init.aliases(provider)
+    t.deepEqual(aliases, {
+      name1: {domain: 'domain1', path: 'path1', auth: {a: 1}}
+    })
+  })
+  it('__path.auth', () => {
+    var provider = {
+      domain1: {
+        __domain: {auth: {a: 1}},
+        path1: {__path: {alias: 'name1', auth: {a: 2}}}
+      }
+    }
+    var aliases = init.aliases(provider)
+    t.deepEqual(aliases, {
+      name1: {domain: 'domain1', path: 'path1', auth: {a: 2}}
+    })
   })
 })
