56dca2910f1811b9ca598402ea4995629610ebbe
simov <simeonvelichkov@gmail.com>
2016-04-10 15:54:29 +0300

    Fix endpoint tests

--- test/endpoint.js
+++ test/endpoint.js
@@ -1,144 +1,107 @@
+'use strict'
 
-var should = require('should')
-var endpoint = require('../lib/endpoint')
-
-
-describe('options', function () {
-  it('all', function () {
-    var endpoints = {all: {get:{a:1}}}
-      , options = {b:2}
-    var result = endpoint.options('endpoint', options, 'get', endpoints)
-    should.deepEqual(result, {a:1, b:2})
-  })
-  it('regex', function () {
-    var endpoints = {regex: {'\\d+': {__endpoint:{regex:true}, get:{a:1}}}}
-      , options = {b:2}
-    var result = endpoint.options('123', options, 'get', endpoints)
-    should.deepEqual(result, {a:1, b:2})
-  })
-  it('str', function () {
-    var endpoints = {str: {string:{get:{a:1}}}}
-      , options = {b:2}
-    var result = endpoint.options('string', options, 'get', endpoints)
-    should.deepEqual(result, {a:1, b:2})
-  })
-})
-
-describe('auth', function () {
-  it('all', function () {
-    var endpoints = {all: {__endpoint:{auth:'a'}}}
-      , auth = endpoint.auth('endpoint', endpoints)
-    auth.should.equal('a')
-  })
-  it('regex', function () {
-    var endpoints = {regex: {'\\d+': {__endpoint:{regex:true, auth:'a'}}}}
-      , auth = endpoint.auth('123', endpoints)
-    auth.should.equal('a')
-  })
-  it('str', function () {
-    var endpoints = {str: {string:{__endpoint:{auth:'a'}}}}
-      , auth = endpoint.auth('string', endpoints)
-    auth.should.equal('a')
-  })
-})
+var t = require('assert')
+var init = require('../lib/init')
+var _endpoint = require('../lib/endpoint')
 
 
-var fixture = {
-  provider: {
-    custom1: {
-      __provider: {
-        oauth: true
-      },
-      'https://domain.com': {
-        __domain: {
-          auth: {qs:{access_token:'[1]'}}
-        },
-        'api/[version]/{endpoint}.[type]': {
-          __path: {
-            alias: ['__default'],
-            version: 'v3'
-          },
-          '*': {
-            all: {
-              headers: {
-                'x-li-format': 'json'
-              }
-            }
-          },
-          // added dynamically below
-          // 'documents': {
-          //   get: {
-          //     encoding: null
-          //   }
-          // },
-          'files\\/\\d+\\/content': {
-            __endpoint: {
-              regex: true
-            },
-            post: {
-              multipart: 'file'
-            }
+describe('endpoint', () => {
+  it('return request options on missing endpoints', () => {
+    var provider = {
+      domain1: {path1: {__path: {alias: 'alias1'}}}
+    }
+    var endpoint = _endpoint(init.aliases(provider))
+    t.deepEqual(
+      endpoint.options('alias1', {b: 2}),
+      {b: 2})
+  })
+
+  describe('types', () => {
+    it('match all', () => {
+      var provider = {
+        domain1: {path1: {__path: {alias: 'alias1'}, '*': {all: {a: 1}}}}
+      }
+      var endpoint = _endpoint(init.aliases(provider))
+      t.deepEqual(
+        endpoint.options('alias1', {b: 2}),
+        {a: 1, b: 2})
+    })
+    it('string', () => {
+      var provider = {
+        domain1: {path1: {__path: {alias: 'alias1'}, 'some/path': {all: {a: 1}}}}
+      }
+      var endpoint = _endpoint(init.aliases(provider))
+      t.deepEqual(
+        endpoint.options('alias1', {url: 'some/path', b: 2}),
+        {url: 'some/path', a: 1, b: 2})
+    })
+    it('regular expression', () => {
+      var provider = {
+        domain1: {path1: {__path: {alias: 'alias1'},
+          'id\/\\d+': {__endpoint: {regex: true}, all: {a: 1}}}}
+      }
+
+      var endpoint = _endpoint(init.aliases(provider))
+      t.deepEqual(
+        endpoint.options('alias1', {url: 'id/1', b: 2}),
+        {url: 'id/1', a: 1, b: 2})
+    })
+  })
+
+  describe('method', () => {
+    it('uppercase method + lowercase config', () => {
+      var provider = {
+        domain1: {path1: {__path: {alias: 'alias1'}, '*': {get: {a: 1}}}}
+      }
+      var endpoint = _endpoint(init.aliases(provider))
+      t.deepEqual(
+        endpoint.options('alias1', {method: 'GET', b: 2}),
+        {method: 'GET', a: 1, b: 2})
+    })
+    it('lowercase method + uppercase config', () => {
+      var provider = {
+        domain1: {path1: {__path: {alias: 'alias1'}, '*': {GET: {a: 1}}}}
+      }
+      var endpoint = _endpoint(init.aliases(provider))
+      t.deepEqual(
+        endpoint.options('alias1', {method: 'get', b: 2}),
+        {method: 'get', a: 1, b: 2})
+    })
+  })
+
+  describe('priority', () => {
+    it('options: 1.request -> 2.endpoint', () => {
+      var provider = {
+        domain1: {path1: {__path: {alias: 'alias1'}, '*': {all: {a: 1}}}}
+      }
+      var endpoint = _endpoint(init.aliases(provider))
+      t.deepEqual(
+        endpoint.options('alias1', {a: 2}),
+        {a: 2})
+    })
+    it('endpoint: 1.regex -> 2.str -> 3.*', () => {
+      var provider = {
+        domain1: {
+          path1: {__path: {alias: 'alias1'},
+            '*': {all: {a: 1}},
+            'some/path': {all: {a: 2}},
+            'id\/\\d+': {__endpoint: {regex: true}, all: {a: 3}}
           }
         }
       }
-    }
-  },
-  alias: {
-    __default: {
-      domain: 'https://domain.com',
-      path: 'api/[version]/{endpoint}.[type]',
-      version: 'v3',
-      auth: {qs:{access_token:'[1]'}},
-      endpoints: {
-        all: {
-          'all': {
-            headers: {
-              'x-li-format': 'json'
-            }
-          }
-        },
-        str: {
-          'documents': {
-            get: {
-              encoding: null
-            }
-          }
-        },
-        regex: {
-          'files\\/\\d+\\/content': {
-            __endpoint: {
-              regex: true
-            },
-            post: {
-              multipart: 'file'
-            }
-          }
-        }
-      }
-    }
-  }
-}
+      var endpoint = _endpoint(init.aliases(provider))
 
-describe('config', function () {
-  it('options', function () {
-    var endpoints = fixture.alias.__default.endpoints
+      t.deepEqual(
+        endpoint.options('alias1', {url: 'something/else', b: 2}),
+        {url: 'something/else', a: 1, b: 2})
 
-    should.deepEqual(
-      endpoint.options('files', {}, 'get', endpoints),
-      {headers:{'x-li-format':'json'}}
-    )
-    should.deepEqual(
-      endpoint.options('documents', {}, 'get', endpoints),
-      {headers:{'x-li-format':'json'}, encoding:null }
-    )
-    should.deepEqual(
-      endpoint.options('files/123/content', {}, 'post', endpoints),
-      {headers:{'x-li-format':'json'}, multipart:'file'}
-    )
-    var options = {headers:{'User-Agent':'Grant', 'x-li-format':'xml'}}
-    should.deepEqual(
-      endpoint.options('files', options, 'get', endpoints),
-      {headers:{'x-li-format':'xml', 'User-Agent':'Grant'}}
-    )
+      t.deepEqual(
+        endpoint.options('alias1', {url: 'some/path', b: 2}),
+        {url: 'some/path', a: 2, b: 2})
+
+      t.deepEqual(
+        endpoint.options('alias1', {url: 'id/1', b: 2}),
+        {url: 'id/1', a: 3, b: 2})
+    })
   })
 })
